6211f027ba5a0eb63ebdbfacb18015b0bd38e1ae
CBS <y826855@naver.com>
2026-09-03 18:24:38 +0900

    fix : 꺠지는 유물이 피해 입을때 블루프린트 사운드 연출이 서버에서만 재생되던 현상 수정

--- Source/NoPhotos/Gameplay/Relic/NPBreakableRelic.cpp
+++ Source/NoPhotos/Gameplay/Relic/NPBreakableRelic.cpp
@@ -165,6 +165,19 @@ void ANPBreakableRelic::HandleDurabilityDamaged(
 			0.0f,
 			1.0f)
 		: 0.0f;
+	MulticastRelicDamaged(
+		Damage,
+		CurrentHealth,
+		MaxHealth,
+		RemainingHealthRatio);
+}
+
+void ANPBreakableRelic::MulticastRelicDamaged_Implementation(
+	const int32 Damage,
+	const int32 CurrentHealth,
+	const int32 MaxHealth,
+	const float RemainingHealthRatio)
+{
 	OnRelicDamaged(
 		Damage,
 		CurrentHealth,
