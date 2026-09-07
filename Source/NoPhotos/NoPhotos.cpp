76316e36cd556b537735ccf51f8fa5e59255acbc
wh1150 <80726310+wh1150@users.noreply.github.com>
2026-09-07 12:19:18 +0900

    fix : 게임 최대 프레임 60으로 제한

--- Source/NoPhotos/NoPhotos.cpp
+++ Source/NoPhotos/NoPhotos.cpp
@@ -3,6 +3,43 @@
 #include "NoPhotos.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, NoPhotos, "NoPhotos" );
+#if !WITH_EDITOR
+#include "Engine/Engine.h"
+#include "GameFramework/GameUserSettings.h"
+#include "Misc/CoreDelegates.h"
+#endif
+
+class FNoPhotosModule : public FDefaultGameModuleImpl
+{
+public:
+#if !WITH_EDITOR
+	virtual void StartupModule() override
+	{
+		FCoreDelegates::OnFEngineLoopInitComplete.AddRaw(this, &FNoPhotosModule::ApplyFrameRateLimit);
+	}
+
+	virtual void ShutdownModule() override
+	{
+		FCoreDelegates::OnFEngineLoopInitComplete.RemoveAll(this);
+	}
+
+private:
+	void ApplyFrameRateLimit()
+	{
+		if (IsRunningDedicatedServer() || !GEngine)
+		{
+			return;
+		}
+
+		if (UGameUserSettings* Settings = GEngine->GetGameUserSettings())
+		{
+			Settings->SetFrameRateLimit(60.0f);
+			Settings->ApplyNonResolutionSettings();
+		}
+	}
+#endif
+};
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FNoPhotosModule, NoPhotos, "NoPhotos" );
 
 DEFINE_LOG_CATEGORY(LogNoPhotos)
