764bd8a5a9edcac81d62a17477610e6b0cc9c481
wh1150 <80726310+wh1150@users.noreply.github.com>
2026-09-07 11:08:28 +0900

    fix : 크래시 해결 시도, FWorldDelegates::OnSeamlessTravelTransition을 구독

--- Source/NoPhotos/Gameplay/Character/NPStablePhysicsPawn.h
+++ Source/NoPhotos/Gameplay/Character/NPStablePhysicsPawn.h
@@ -15,6 +15,7 @@ class USkeletalMeshComponent;
 class USoundAttenuation;
 class USoundBase;
 class USpringArmComponent;
+class UWorld;
 class UNPStablePhysicsDebugComponent;
 class UNPStablePhysicsGrabComponent;
 class UNPStablePhysicsMovementComponent;
@@ -249,6 +250,7 @@ protected:
 	FName RightHandBoneName = TEXT("hand_r");
 
 private:
+	void HandleSeamlessTravelTransition(UWorld* TransitioningWorld);
 	void WaitForTemporaryRagdollSettle();
 	void FinishTemporaryRagdollRecovery();
 	void FinishTemporaryRagdollInputDelay();
@@ -279,6 +281,7 @@ private:
 	bool bTemporaryRagdollActive = false;
 	bool bTemporaryRagdollRecoveryActive = false;
 	bool bRelicSwingActive = false;
+	FDelegateHandle SeamlessTravelTransitionHandle;
 	FTimerHandle TemporaryRagdollTimer;
 	FTimerHandle TemporaryRagdollRecoveryTimer;
 	FTimerHandle TemporaryRagdollInputDelayTimer;
