3468eb2b2ea691f4ed90e1d9412b1dc563070d3e
Quanx0411 <quanx0411@gmail.com>
2026-09-08 15:51:27 +0900

    fix:병합하면서 UNPAbilitySystemComponent의 public/private 부분 잘못된 거 수정이요

--- Source/NoPhotos/Gameplay/AbilitySystem/NPAbilitySystemComponent.h
+++ Source/NoPhotos/Gameplay/AbilitySystem/NPAbilitySystemComponent.h
@@ -24,6 +24,10 @@ public:
 	/** 조준 입력 라우터가 호출합니다. */
 	void ActivateRelicAimAbility();
 	void CancelRelicAimAbility();
+	void ActivateRelicFireAbility();
+	void TogglePhotoAimAbility();
+	void CancelPhotoAimAbility();
+	void ActivatePhotoShotAbility();
 	float GetLavaBurnDuration() const { return LavaBurnDuration; }
 
 protected:
@@ -35,11 +39,6 @@ protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Lava",
 		meta=(DisplayName="불 연출 지속 시간", ClampMin="0.01", Units="s"))
 	float LavaBurnDuration = 0.5f;
-	void ActivateRelicFireAbility();
-	void TogglePhotoAimAbility();
-	void CancelPhotoAimAbility();
-	void ActivatePhotoShotAbility();
-
 private:
 	void ActivateRelicUseAbility();
 	void ClearHeldRelicAbilities();
