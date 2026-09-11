affbfb1ac79458652fe38baf0ab48f774f2f238c
wh1150 <80726310+wh1150@users.noreply.github.com>
2026-09-11 20:40:32 +0900

    resource : 네모방 용암 사운드, 황금 사과 그랩 시 벽 막히는 기믹 사운드 추가

--- Source/NoPhotos/Gameplay/AbilitySystem/GameplayCue/NPLavaBurningGameplayCue.cpp
+++ Source/NoPhotos/Gameplay/AbilitySystem/GameplayCue/NPLavaBurningGameplayCue.cpp
@@ -1,9 +1,13 @@
 #include "Gameplay/AbilitySystem/GameplayCue/NPLavaBurningGameplayCue.h"
 
+#include "Components/AudioComponent.h"
 #include "NiagaraComponent.h"
+#include "TimerManager.h"
 
 ANPLavaBurningGameplayCue::ANPLavaBurningGameplayCue()
 {
+	bAllowMultipleWhileActiveEvents = true;
+
 	LavaFireLeft = CreateDefaultSubobject<UNiagaraComponent>(TEXT("LavaFireLeft"));
 	LavaFireLeft->SetupAttachment(SceneRoot);
 	LavaFireLeft->SetRelativeLocation(FVector(0.0f, -25.0f, 100.0f));
@@ -13,6 +17,11 @@ ANPLavaBurningGameplayCue::ANPLavaBurningGameplayCue()
 	LavaFireRight->SetupAttachment(SceneRoot);
 	LavaFireRight->SetRelativeLocation(FVector(0.0f, 25.0f, 100.0f));
 	LavaFireRight->SetAutoActivate(false);
+
+	LavaAudio = CreateDefaultSubobject<UAudioComponent>(TEXT("LavaAudio"));
+	LavaAudio->SetupAttachment(SceneRoot);
+	LavaAudio->SetRelativeLocation(FVector(0.0f, 0.0f, 100.0f));
+	LavaAudio->SetAutoActivate(false);
 }
 
 bool ANPLavaBurningGameplayCue::WhileActive_Implementation(
@@ -54,4 +63,32 @@ void ANPLavaBurningGameplayCue::SetLavaActive(bool bActive)
 			Fire->DeactivateImmediate();
 		}
 	}
+
+	if (bActive)
+	{
+		RestartLavaSound();
+	}
+	else
+	{
+		StopLavaSound();
+	}
+}
+
+void ANPLavaBurningGameplayCue::RestartLavaSound()
+{
+	GetWorldTimerManager().ClearTimer(LavaSoundStopTimerHandle);
+	LavaAudio->Stop();
+	LavaAudio->Play(FMath::Max(0.0f, LavaSoundStartTime));
+	GetWorldTimerManager().SetTimer(
+		LavaSoundStopTimerHandle,
+		this,
+		&ANPLavaBurningGameplayCue::StopLavaSound,
+		FMath::Max(0.01f, LavaSoundDuration),
+		false);
+}
+
+void ANPLavaBurningGameplayCue::StopLavaSound()
+{
+	GetWorldTimerManager().ClearTimer(LavaSoundStopTimerHandle);
+	LavaAudio->Stop();
 }
