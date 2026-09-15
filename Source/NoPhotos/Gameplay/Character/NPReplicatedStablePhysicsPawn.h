aad98f7ffb9c50901eafb8d81073b5158a73dcb4
CBS <y826855@naver.com>
2026-09-15 11:49:32 +0900

    feat : 캐릭터 그랩 가시화  - 옛날 그랩 사운드 제거  - 그랩 사운드 볼륨 업

--- Source/NoPhotos/Gameplay/Character/NPReplicatedStablePhysicsPawn.h
+++ Source/NoPhotos/Gameplay/Character/NPReplicatedStablePhysicsPawn.h
@@ -9,6 +9,7 @@
 #include "NPReplicatedStablePhysicsPawn.generated.h"
 
 class UPrimitiveComponent;
+class UStaticMeshComponent;
 class UAbilitySystemComponent;
 class AController;
 class FLifetimeProperty;
@@ -106,6 +107,9 @@ public:
 		FVector_NetQuantizeNormal CameraForward);
 
 protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Grab|Visual")
+	TObjectPtr<UStaticMeshComponent> GrabPreviewMesh;
+
 	virtual void BeginPlay() override;
 	virtual void CompleteTemporaryRagdollRecovery() override;
 	virtual void PossessedBy(AController* NewController) override;
@@ -128,6 +132,9 @@ protected:
 	void OnGrabConstraintBroken();
 
 private:
+	void UpdateGrabPreview();
+	FVector GrabPreviewBaseScale = FVector::OneVector;
+
 	FActiveGameplayEffectHandle LeaderEffectHandle;
 
 	static constexpr float ViewRotationSendInterval = 0.05f;
